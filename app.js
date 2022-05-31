19576db3dc78e33de1ab2bdd9c6778da68e797b1
EvoFox <foulkes.joshua@live.co.uk>
2022-05-31 15:54:17 +0100

    refactoring and addition of logic
    Co-authored-by: WiseThoughts <WiseThoughts@users.noreply.github.com>
    Co-authored-by: Olly-Thorburn <Olly-Thorburn@users.noreply.github.com>

--- app.js
+++ app.js
@@ -10,37 +10,45 @@ const {
     Dog
 } = require("./cyberPet");
 
-// 
-let myPet = "";
+// Array for typeOfPetInquirer, should be analogous
+// to the imported pets from the cyberPet module above
+potentialPets = [{
+        key: "A",
+        name: "Cat",
+        value: "cat",
+    },
+    {
+        key: "B",
+        name: "Dog",
+        value: "dog",
+    }
+]
+
 
 try {
-    const start = async () => {
+    let myPet = "";
+    let options = [];
+
+    const start = async (callback) => {
         //Create first inquirer prompt, to set the type of pet
         const typeOfPet = await inquirer.prompt({
             type: "list",
             name: "typeOfPet",
             message: "What kind of pet would you like?",
-            choices: [{
-                    key: "A",
-                    name: "Cat",
-                    value: "cat",
-                },
-                {
-                    key: "B",
-                    name: "Dog",
-                    value: "dog",
-                }
-            ]
+            choices: potentialPets
         });
         // Output of above:
         // { typeOfPet: 'cat' }
         // or
         // { typeOfPet: 'dog' }
-        // Two ways to access this
+        // Two ways to access this: typeOfPet["typeOfPet"] 
+        // or
+        // typeOfPet.typeOfPet
 
+        if (debug) {
+            console.log(typeOfPet)
+        }
 
-
-        console.log(typeOfPet)
         // Create second inquirer prompt, to set the name of the pet
         const petName = await inquirer.prompt({
             type: "input",
@@ -51,16 +59,123 @@ try {
         // Check the type of pet and create new pet based on result
         if (typeOfPet.typeOfPet === "cat") {
             myPet = new Cat(petName.nameOfPet);
-            console.log(myPet);
-        } else if (typeOfPet["typeOfPet"] === "dog") {
+            options = [{
+                    key: "A",
+                    name: "Give Food",
+                    value: "feed"
+                },
+                {
+                    key: "B",
+                    name: "Give Water",
+                    value: "water",
+                },
+                {
+                    key: "C",
+                    name: "Play with Wool",
+                    value: "playWool"
+                },
+                {
+                    key: "D",
+                    name: "Let Sleep",
+                    value: "sleep",
+                }
+            ]
+            // console.log(myPet);
+        } else if (typeOfPet.typeOfPet === "dog") {
             myPet = new Dog(petName.nameOfPet);
-            console.log(myPet);
+            options = [{
+                    key: "A",
+                    name: "Give Food",
+                    value: "feed"
+                },
+                {
+                    key: "B",
+                    name: "Give Water",
+                    value: "water",
+                },
+                {
+                    key: "C",
+                    name: "Play Ball",
+                    value: "playBall"
+                },
+                {
+                    key: "D",
+                    name: "Walk",
+                    value: "walk",
+                }
+            ]
+            // console.log(myPet);
         }
+        timer()
+        callback(game)
     }
 
+    const timer = async () => {
+        await setInterval(() => {
+            myPet.decreaseStats();
+        }, 15000);
 
-    start()
-    console.log(myPet)
+        // This causes some graphical issues on the terminal, though prevents a memory leak
+        // from calling the game function too many times.
+        myPet.checkAlive();
+        game(game);
+    }
+
+    const game = async (callback) => {
+        const activity = await inquirer.prompt({
+            type: "list",
+            name: "whatActivity",
+            message: `What do you want to do with ${myPet.name}?`,
+            choices: options,
+        });
+
+        if (debug) {
+            console.log(activity.whatActivity)
+            console.log(options);
+        }
+
+        // Check what activity was chosen.
+        switch (activity.whatActivity) {
+            case "feed":
+                myPet.eat();
+                break;
+            case "drink":
+                myPet.drink();
+                break;
+            case "playWool":
+                myPet.playWool();
+                break;
+            case "sleep":
+                myPet.sleep();
+                break;
+            case "playBall":
+                myPet.playBall();
+                break;
+            case "walk":
+                myPet.walk();
+                break;
+        }
+        
+
+        callback(game)
+
+
+    }
+
+    // const mainGame = async () => {
+    //     setInterval(() => {
+    //         
+
+    //         myPet.decreaseStats();
+    //     }, 5000);
+
+
+
+
+    // }
+
+    start(game);
+
 
 } catch (error) {
     console.log("Error fetching data! ", error)
--- cyberPet.js
+++ cyberPet.js
@@ -7,15 +7,46 @@ class Animal {
         this.thirst = 100;
     }
     eat() {
-        // TODO: add flavour text to this function
+
+        console.log(`\n\n${this.name} takes a moment to eat, recovering its thirst to ${this.thirst}.`);
         this.hunger += 10;
     }
     drink() {
         // TODO: add flavour text to this function
+        console.log(`\n\n${this.name} takes a drink of water, recovering its thirst to ${this.thirst}.`);
         this.thirst += 10;
     }
     checkAlive() {
-        // TODO: Work out the specifics of how we will check the stats and if a virtual pet is still alive
+        if (this.health <= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    decreaseStats() {
+        if (this.hunger < 100) {
+            this.health -= 5;
+            console.log(`\n\nIt's been a while since ${this.name} ate, its health has gone down by 5!`);
+        } else {
+            this.health += 5
+            console.log(`\n\nYou're taking great care of ${this.name}, its health has gone up by 5!`);
+        }
+        if (this.thirst < 100) {
+            this.health -= 5;
+            console.log(`\n\nIt's been a while since ${this.name} ate, its health has gone down by 5!`);
+        } else {
+            this.health += 5
+            console.log(`\n\nYou're taking great care of ${this.name}, its health has gone up by 5!`);
+        }
+
+        let decreaseThirst = Math.random() * (15 - 5) + 15;
+        let decreaseHunger = Math.random() * (15 - 5) + 15;
+
+        this.thirst -= decreaseThirst;
+        this.hunger -= decreaseHunger;
+
+        console.log(`\n\n${this.name}'s stats are now: `);
+        console.table(this)
     }
 
 }
@@ -27,10 +58,12 @@ class Dog extends Animal {
     }
     playBall() {
         this.health += 10;
-        this.hunger -= 10;
+        this.hunger -= 20;
+        console.log("\n\nball")
     }
     walk() {
-
+        this.health += 10;
+        this.thirst -= 20;
     }
 
 }
@@ -40,7 +73,14 @@ class Cat extends Animal {
     constructor(name) {
         super(name);
     }
-
+    playWool() {
+        this.health += 10;
+        this.hunger -= 20;
+    }
+    sleep() {
+        this.health += 20;
+        this.hunger -= 10;
+    }
 }
 
 module.exports = {
